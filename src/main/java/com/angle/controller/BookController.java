f1f749241cd3f3d333ed25626f8f29837966e762
hejinl <hejinl@mail.open.com.cn>
2022-10-17 15:20:05 +0800

    统一返回格式

--- src/main/java/com/angle/controller/BookController.java
+++ src/main/java/com/angle/controller/BookController.java
@@ -12,33 +12,43 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController {
 
-
     @Autowired
     private BookService bookService;
 
     @PostMapping
-    public boolean save(@RequestBody Book book) {
-        return bookService.save(book);
+    public Result save(@RequestBody Book book) {
+        System.out.println("进来了" + book.toString());
+        boolean flag = bookService.save(book);
+        System.out.println("保存成功" + flag);
+        return new Result(flag ? Code.SAVE_OK : Code.SAVE_ERR, flag);
     }
 
     @PutMapping
-    public boolean upDate(@RequestBody Book book) {
-        return bookService.upDate(book);
+    public Result upDate(@RequestBody Book book) {
+        boolean flag = bookService.upDate(book);
+        return new Result(flag ? Code.UPDATE_OK : Code.UPDATE_ERR, flag);
     }
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable int id) {
-        return bookService.delete(id);
+    public Result delete(@PathVariable int id) {
+        boolean flag = bookService.delete(id);
+        return new Result(flag ? Code.DELETE_OK : Code.DELETE_ERR, flag);
     }
 
     @GetMapping("/{id}")
-    public Book getById(@PathVariable int id) {
-        return bookService.getById(id);
+    public Result getById(@PathVariable int id) {
+        Book book = bookService.getById(id);
+        int code = book != null ? Code.GET_OK : Code.GET_ERR;
+        String msg = book != null ? "" : "数据查询失败,请稍后再试";
+        return new Result(code, book, msg);
     }
 
     @GetMapping
-    public List<Book> getAll() {
-        return bookService.getAll();
-    }
+    public Result getAll() {
+        List<Book> books = bookService.getAll();
+        int code = books != null ? Code.GET_OK : Code.GET_ERR;
+        String msg = books != null ? "" : "数据查询失败,请稍后再试";
+        return new Result(code, books, msg);
 
+    }
 }
